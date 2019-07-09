4b281140f29aa28b5bd84ca8eb54b998e464bc9c
ruoran <fengruoshuiran@foxmail.com>
2019-07-09 16:54:45 +0800

    - Add event, actions - Finish Hide module

--- src/main/kotlin/yuyuko/remake/YuyukoRemake.kt
+++ src/main/kotlin/yuyuko/remake/YuyukoRemake.kt
@@ -3,10 +3,17 @@ package yuyuko.remake
 import basemod.BaseMod
 import basemod.interfaces.*
 import  com.evacipated.cardcrawl.modthespire.lib.SpireInitializer
+import com.megacrit.cardcrawl.cards.AbstractCard
 import yuyuko.remake.customs.Customs
+import yuyuko.remake.events.Events
 
 @SpireInitializer
-object YuyukoRemake :  EditCharactersSubscriber, EditStringsSubscriber, EditCardsSubscriber, EditRelicsSubscriber{
+object YuyukoRemake :
+        EditCharactersSubscriber,
+        EditStringsSubscriber,
+        EditCardsSubscriber,
+        EditRelicsSubscriber,
+        PostDrawSubscriber {
     @JvmStatic fun initialize() {}
 
     init {
@@ -21,4 +28,6 @@ object YuyukoRemake :  EditCharactersSubscriber, EditStringsSubscriber, EditCard
     override fun receiveEditCards()  = Customs.setCards()
 
     override fun receiveEditRelics() = Customs.setRelics()
+
+    override fun receivePostDraw(abstractCard: AbstractCard) = Events.onDraw(abstractCard)
 }
